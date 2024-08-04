79498ef1bf13278759c521186acafba477108a46
Horikawa <m21137@g.metro-cit.ac.jp>
2024-08-04 14:55:30 +0900

    cpmmit5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import  'create.dart';
 import 'in.dart';
 
@@ -115,5 +114,7 @@ class _UserPageState extends State<UserPage> {
     );
   }
 }
+
+
 
 
